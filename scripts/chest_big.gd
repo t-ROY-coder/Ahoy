2efb500e4ea227c4f9a187b5c4c2183a96739f40
Tanumon <roytanumon@gmail.com>
2024-07-26 21:08:18 +0530

    updated sounds

--- scripts/chest_big.gd
+++ scripts/chest_big.gd
@@ -1,14 +1,18 @@
 extends Area2D
 
-@onready var audio_stream_player = $AudioStreamPlayer2D
+@onready var audio_stream_player = $ChestOpenSound
+@onready var no_key_sound = $NoKeySound
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var game_manager = %GameManager
 var is_reachable = false
 var is_chest_open = false
 
 func _process(delta):
-	if game_manager.key_count > 0 && Input.is_action_just_pressed("interact") && is_reachable && !is_chest_open:
-		open_chest()
+	if Input.is_action_just_pressed("interact") && is_reachable && !is_chest_open:
+		if game_manager.key_count > 0:
+			open_chest()
+		else:
+			no_key_sound.play()
 		
 
 func open_chest():
