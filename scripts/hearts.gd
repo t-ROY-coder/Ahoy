76caa301bb86543f76f7f76998bdfb64e8a7c238
Tanumon <roytanumon@gmail.com>
2024-07-28 02:17:24 +0530

    added empty hearts for lost hp

--- scripts/hearts.gd
+++ scripts/hearts.gd
@@ -2,8 +2,8 @@ extends HBoxContainer
 
 func update_hearts(hp):
 	if hp >= 1:
-		get_node("Heart").visible = true
+		get_node("Heart").texture.region = Rect2(0, 0, 32, 32)
 		if hp >= 2:
-			get_node("Heart2").visible = true
+			get_node("Heart2").texture.region = Rect2(0, 0, 32, 32)
 			if hp >= 3:
-				get_node("Heart3").visible = true
+				get_node("Heart3").texture.region = Rect2(0, 0, 32, 32)
