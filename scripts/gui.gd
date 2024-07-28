6ec314639acefd9ad9715bc3abb460a276eb0837
Tanumon <roytanumon@gmail.com>
2024-07-28 20:00:08 +0530

    big fix

--- scripts/gui.gd
+++ scripts/gui.gd
@@ -22,10 +22,14 @@ func update_health(count):
 	
 func on_level_complete(coin_count):
 	level_complete.visible = true
+	game_over.visible = false
+	start_menu.visible = false
 	level_complete.get_node("Scoreboard").text = "Coins collected: " + str(coin_count)
 	
 func on_game_over():
+	level_complete.visible = false
 	game_over.visible = true
+	start_menu.visible = false
 
 func _on_restart_button_pressed():
 	print("RESTART")
@@ -34,6 +38,8 @@ func _on_restart_button_pressed():
 
 func show_main_menu():
 	color_rect.visible = true
+	level_complete.visible = false
+	game_over.visible = false
 	start_menu.visible = true
 
 func _on_start_button_pressed():
